8771b46e68470fda8f52faa22435143792982d55
leonid <lkonontsov@hotmail.com>
2020-01-26 10:38:23 +0300

    added: isMoving property

--- utils.lua
+++ utils.lua
@@ -1,8 +1,8 @@
 local utils = {}
 
 function utils.approximate(x, y, ox, oy, speed)
-  local vx = -((y - oy) / math.sqrt((y-oy)*(y-oy) + (y-oy)*(y-oy))) * speed
-  local vy = -((x - ox) / math.sqrt((x-ox)*(x-ox) + (y-oy)*(y-oy))) * speed
+  local vy = -((y - oy) / math.sqrt((y-oy)*(y-oy) + (y-oy)*(y-oy))) * speed
+  local vx = -((x - ox) / math.sqrt((x-ox)*(x-ox) + (y-oy)*(y-oy))) * speed
   return y+vy, x+vx
 end
   
